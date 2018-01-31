db43e9bebe6dc2f0709173075b6d1ff4febff30f
James Quinn <flash.jaq@gmail.com>
2018-01-31 15:32:14 +0000

    I3: Guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-num = rand(100).to_s
+randy = rand(100).to_s
 
 get '/' do
-	erb :index, :locals => {:num => num}
+	guess = params["guess"].to_i
+	msg = getMsg(guess.to_i, randy.to_i)
+	erb :index, :locals => {:randy => randy, :msg => msg}
+end
+
+
+def getMsg(guess, randy)
+	diff = guess-randy
+	puts "diff is " + diff.to_s
+	if(diff == 0)
+		return msg = guess.to_s + ' is correct'
+	elsif(diff > 0)
+		if(diff>10)
+			return msg = guess.to_s + ' is way too high'
+		else
+			return msg = guess.to_s + ' is too high'
+		end
+	elsif(diff<-10)
+		return msg = guess.to_s + ' is way too low'
+	else
+		return msg = guess.to_s + ' is too low'#wrong
+	end
+	return "REEE"
 end
